44b4454e216f831efb4789f3de521cbd71349ca7
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-02-06 20:17:30 +0530

    Update index.tsx
    Signed-off-by: APPARAO BALKUNDE <apparaobalkunde901@gmail.com>

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// टीप: इथून आपण डेटाबेस कनेक्शनची सुरुवात करतोय
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
