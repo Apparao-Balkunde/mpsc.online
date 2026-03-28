e87ab9c28e4f9104395e9ce20107167f73f99703
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-03-29 00:03:59 +0530

    Update server.js
    Signed-off-by: APPARAO BALKUNDE <apparaobalkunde901@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import compression from 'compression';
+import 'dotenv/config'; // ही ओळ सर्वात वर हवी!
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
