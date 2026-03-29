c78b07f2f461969c97a5c33d934d09af922ef093
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-03-29 12:01:33 +0530

    Fix import case for Cloudsync module
    Signed-off-by: APPARAO BALKUNDE <apparaobalkunde901@gmail.com>

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ import { MoreMenu } from './components/MoreMenu';
 import { PWAPrompt } from './components/PWAPrompt';
 import { useAuth, signOut } from './useauth';
 import { Result } from './components/Result';
-import { pullProgressFromCloud, pushProgressToCloud, startAutoSync } from './cloudsync';
+import { pullProgressFromCloud, pushProgressToCloud, startAutoSync } from './Cloudsync';
 import { Heart } from 'lucide-react';
 import {
   History, BookOpen, Trophy, Newspaper, ShieldCheck,
