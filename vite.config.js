ebcaf6700a1bc8c1ea2592615651187a0d80bd1b
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-04-06 21:50:25 +0530

    Update vite.config.js
    Signed-off-by: APPARAO BALKUNDE <apparaobalkunde901@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // नवीन प्लगइन ॲड केला
+// खालील ओळ कमेंट कर (तात्पुरती)
+// import tailwindcss from '@tailwindcss/vite'; 
 import path from 'path';
 
 export default defineConfig({
-  // Tailwind v4 साठी tailwindcss() प्लगइन इथे टाकला आहे
-  plugins: [react(), tailwindcss()],
+  // प्लगइन्समधून tailwindcss() काढून टाक
+  plugins: [react()], 
   
   base: '/', 
   resolve: {
@@ -16,13 +17,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
   },
   server: {
     port: 5173,
