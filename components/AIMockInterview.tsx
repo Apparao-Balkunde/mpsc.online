903a438108b4ed55e8a3926726f96138691d520d
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-04-12 16:56:44 +0530

    Update AIMockInterview.tsx
    Signed-off-by: APPARAO BALKUNDE <apparaobalkunde901@gmail.com>

--- components/AIMockInterview.tsx
+++ components/AIMockInterview.tsx
@@ -41,7 +41,7 @@ export const AIMockInterview: React.FC<Props> = ({ onBack }) => {
     if (!window.speechSynthesis) return;
     window.speechSynthesis.cancel();
     const utt = new SpeechSynthesisUtterance(text);
-    utt.lang = 'hi-IN'; utt.rate = 0.9;
+    utt.lang = 'mr-IN'; utt.rate = 0.9;
     window.speechSynthesis.speak(utt);
   };
 
